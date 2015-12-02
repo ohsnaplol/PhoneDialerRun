4d27a1ce21f784fb5cfa743ce10412719fa5b6cc
ohsnaplol <emyb@bellsouth.net>
2015-12-02 12:28:27 -0500

    phoneNumGen removal
    replaced the entire phoneNumGen method with a single line of code found
    at 131

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.Random;
 import java.util.Scanner;
+
+
 
 
 //java fx stuff
@@ -12,15 +15,19 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public abstract class Main extends Application implements EventHandler<ActionEvent> {
 	static int difficulty;
 
-
+	/*
+	 * Get Int() makes sure the input is between 1 and 8 and isn't a string
+	 */
 	public static int getInt () {
+		@SuppressWarnings("resource")
 		Scanner keyboard = new Scanner(System.in);
-		int num = 5;
+		int num = 5; //default difficulty
 		boolean goodInput = false;
 		do {
+			//Makes sure the input isn't a String
 			try {
 				num = keyboard.nextInt();
 			} catch (Exception e){
@@ -37,7 +44,11 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 		return num;
 	}
 
+	/*
+	 * getIntGame makes sure that what the user types during gameplay is only numbers
+	 */
 	public static int getIntGame() {
+		@SuppressWarnings("resource")
 		Scanner keyboard = new Scanner(System.in);
 		int num = 0;
 		boolean goodInput = false;
@@ -46,7 +57,6 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 				num = keyboard.nextInt();
 			} catch (Exception e){
 				System.out.println("Not an Integer");
-				break;
 			}
 			goodInput = true;
 		} while (!goodInput);
@@ -59,36 +69,12 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 		System.out.println("Difficulty set to " + difficulty);
 	}
 
-	public static int phoneNumberGen(int difficulty) {
-		int multiple = 0;
-		switch (difficulty) {
-		case 1: multiple = 10;
-		break;
-		case 2: multiple = 1000;
-		break;
-		case 3: multiple = 10000;
-		break;
-		case 4: multiple = 100000;
-		break;
-		case 5: multiple = 1000000;
-		break;
-		case 6: multiple = 10000000;
-		break;
-		case 7: multiple = 100000000;
-		break;
-		case 8: multiple = 1000000000;
-		break;
-		}
-		int number = (int)(Math.random() * multiple);
-		return number;
-	}
-	
+	//GUI Experimentation
 	Stage window;
 	Scene difficultySelect, game;
-	//GUI Experimentation
 	public void start(Stage primaryStage) {
 		window = primaryStage;
-		
+
 		//button 1
 		Label intro = new Label("Welcome to Speed Dialer!\nSelect a difficulty");
 		Button button1 = new Button("1");
@@ -97,7 +83,7 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 			window.setScene(game);
 			System.out.println(difficulty);
 		});
-		
+
 		//button 2
 		Button button2 = new Button("2");
 		button2.setOnAction(e -> {
@@ -105,44 +91,44 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 			window.setScene(game);
 			System.out.println(difficulty);
 		});
-		
+
 		//quit button
 		Button quitButton = new Button("back");
 		quitButton.setOnAction(e -> window.setScene(difficultySelect));
-		
+
 		//Layout1 - children in vertical column (CHOOSE DIFFICULTY)
 		VBox layout1 = new VBox(20);
 		layout1.getChildren().addAll(intro, button1, button2);
 		difficultySelect = new Scene(layout1, 300,400);
-		
+
 		//Layout 2
 		StackPane layout2 = new StackPane();
 		layout2.getChildren().add(quitButton);
 		game = new Scene(layout2,600,300);
-		
+
 		//Display scene1
 		window.setScene(difficultySelect);
 		window.setTitle("Speed Dialer");
 		window.show();
-		
+
 	}
 	//end GUI Experimentation
 
 	public static void main(String[] args) throws InterruptedException {
-		launch(args); //Comment out to run w/o GUI
 		boolean run = true;
+		double highScore = 999999999;
+		double currentScore = 0;
+		//launch(args); //Comment out to run w/o GUI
 		System.out.println("Welcome to SpeedDialer!");
-		int highScore = 0;
-		int currentScore = 0;
 		do {
 			introduction();
 			Thread.sleep(1000);
 			System.out.println("Enter the following number as fast as possible:");
 			Thread.sleep(2000);
-			System.out.print("Ready..?   ");
+			System.out.print("Ready..?  ");
 			Thread.sleep(1000);
 			System.out.println("GO!");
-			int theNumber = phoneNumberGen(difficulty);
+			int theNumber = (int)(Math.random() * Math.pow(10, difficulty));
 			System.out.println(theNumber);
 			int response;
 			double timeStart = System.currentTimeMillis();
@@ -150,14 +136,14 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 				response = getIntGame();
 			} while (response!=theNumber);
 			double timeEnd = System.currentTimeMillis();
-			//Calculates score
-			currentScore = (int)(10000 - (timeStart - timeEnd) * -1);
-			if (currentScore > highScore) {
+			//Calculates score in ms
+			currentScore = timeEnd - timeStart;
+			if (currentScore < highScore) {
 				highScore = currentScore;
-				System.out.println("New high score! " + highScore);
+				System.out.println("New high score! " + highScore + " ms");
 			} else {
-				System.out.println("Your score: " + currentScore);
-				System.out.println("High score: " + highScore);
+				System.out.println("Your score: " + currentScore + " ms");
+				System.out.println("High score: " + highScore + " ms");
 			}
 		} while (run);
 	}
